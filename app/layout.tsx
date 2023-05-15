b9dc7e856422c9a45fe2c85a42f83f98deee9709
Maralena86 <mari16cobain@msn.com>
2023-05-15 21:41:22 +0200

    changes in style

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       
       <body className={inter.className}><nav>
-        <ul className="flex  gap-1">
+        <ul className="flex  gap-5 p-5">
           {links.map(({label, route})=>(
             <li key={route}>
               <Link href={route}>{label}</Link>
